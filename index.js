f3be39f579a5c8068fd8af9050c69f881194ac32
Malissa Bishop <malissambishop7@gmail.com>
2021-10-13 10:03:50 -0700

    commit tree de2a6d39572812974daae85f101bbdc0bd02a34b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork();
+
+function wrapAdjective(flare = '*') {
+    const innerFunction = function(adj = 'special') {
+        return `You are ${flare}${adj}${flare}!`;
+    }
+    return innerFunction;
+}
